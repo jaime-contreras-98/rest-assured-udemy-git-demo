377d42c68799c4a382a073da66b65173ac524c59
Jaime Alberto Contreras Verduzco <jaime.contreras@globant.com>
2024-01-04 22:21:49 -0700

    new branch changes

---
+++ src/test/java/step/definitions/AnotherAddition.java
@@ -0,0 +1,10 @@
+package step.definitions;
+
+public class AnotherAddition {
+
+	public static void main(String[] args) {
+		NewAddition newObj = new NewAddition();
+		System.out.println(newObj.toString());
+	}
+
+}
--- src/test/java/step/definitions/NewAddition.java
+++ src/test/java/step/definitions/NewAddition.java
@@ -5,6 +5,7 @@ public class NewAddition {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello2");
+		System.out.println("Hello3");
 	}
 
 }
